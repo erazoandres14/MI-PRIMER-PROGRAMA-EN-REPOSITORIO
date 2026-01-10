fafb1f8e1e3d42cc923943bd3c239f66f384d7aa
erazoandres14 <soler1408@gmail.com>
2026-01-10 11:15:41 -0500

    Create main.py
    Mi primera version el programa

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+    "CRINGE": "Algo excepcionalmente raro o embarazoso",
+    "LOL": "Una respuesta común a algo gracioso",
+    "HOLIWIS" : "Una forma tierna de decir hola"
+}
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
+    print(meme_dict[word])
+else:
+    # ¿Qué hacer si no se encuentra la palabra?
+    print("Esa palabra no existe en el diccionario : " , word)
