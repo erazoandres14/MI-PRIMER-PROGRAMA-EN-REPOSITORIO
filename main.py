b36f2ed30f5d6ee87bff1e85776c26662bfec601
erazoandres14 <soler1408@gmail.com>
2026-02-25 18:14:22 -0500

    Update main.py
    En la clase 4 de python pro, ingrese un bot aqui.

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-meme_dict = {
-    "CRINGE": "Algo excepcionalmente raro o embarazoso",
-    "LOL": "Una respuesta común a algo gracioso",
-    "HOLIWIS" : "Una forma tierna de decir hola"
-}
+import discord
+from discord.ext import commands
 
-word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
 
-if word in meme_dict.keys():
-    # ¿Qué debemos hacer si se encuentra la palabra?
-    print(meme_dict[word])
-else:
-    # ¿Qué hacer si no se encuentra la palabra?
-    print("Esa palabra no existe en el diccionario : " , word)
+# Configuración de intents
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+# Prefijo de comandos
+bot = commands.Bot(command_prefix="$", intents=intents)
+
+# Evento cuando el bot se conecta
+@bot.event
+async def on_ready():
+    print(f"✅ Bot conectado como {bot.user}")
+
+
+@bot.command()
+async def saludar(ctx):
+    await ctx.send("Hola")
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+bot.run("")
